72591f0c18bf6cc0c93a7d47db9fab788e98dc17
ayeshafatima <ayeshafatima@localhost>
2022-02-23 13:50:10 +0530

    java Expressions added in to project 08-02-2022

---
+++  08-02-2022 Java Expressions/src/com/javaExpressions/Example1.java
@@ -0,0 +1,20 @@
+package com.javaExpressions;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+	 int a;
+	 //Expression
+	 a=10+15;
+	 System.out.println("a:"+a);
+	 //Expression
+	 a=20-10;
+	 System.out.println("a:"+a);
+	 a=2*5;
+	 System.out.println("a:"+a);
+	 a=10/2;
+	 System.out.println("a:"+a);
+	 
+	}
+
+}
